822f7cb5963f692a0ab95531b9807ccf6c736a1d
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-06-27 11:17:43 +0300

    feat: choose country for phone input

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
+// import intlTelInput from 'intl-tel-input'
+import intlTelInput from "intl-tel-input/intlTelInputWithUtils"
+
+import 'intl-tel-input/build/css/intlTelInput.css'
 import './scss/main.scss'
 
 const from = document.querySelector('.form-block__form')
+
+const nameInput = document.querySelector('.form-block__name')
+const phoneInput = document.querySelector('.form-block__phone')
+const emailInput = document.querySelector('.form-block__email')
+
+const nameError = document.getElementById('name-error')
+const phoneError = document.getElementById('phone-error')
+const emailError = document.getElementById('email-error')
+
+const iti = intlTelInput(phoneInput, {
+  initialCountry: 'ua',
+  strictMode: true
+})
 
 from.addEventListener('submit', (event) => {
   event.preventDefault()
 
   let isValid = true
 
-  const nameInput = document.querySelector('.form-block__name')
-  const phoneInput = document.querySelector('.form-block__phone')
-  const emailInput = document.querySelector('.form-block__email')
-
-  const nameError = document.getElementById('name-error')
-  const phoneError = document.getElementById('phone-error')
-  const emailError = document.getElementById('email-error')
-
-  const phoneRegex = /^\+\d{12}$/
+  const phoneRegex = /\d{7,14}$/
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
   if (!nameInput.value.trim()) {
     nameError.style.display = 'block'
     nameError.innerText = 'Поле не заполнено'
-    isValid = false
   } else {
     nameError.style.display = 'none'
   }
@@ -29,11 +37,9 @@ from.addEventListener('submit', (event) => {
   if (!phoneInput.value.trim()) {
     phoneError.style.display = 'block'
     phoneError.innerText = 'Поле не заполнено'
-    isValid = false
   } else if (!phoneRegex.test(phoneInput.value.trim())) {
     phoneError.style.display = 'block'
     phoneError.innerText = 'Поле должно быть заполнено в международном формате'
-    isValid = false
   } else {
     phoneError.style.display = 'none'
   }
@@ -41,18 +47,17 @@ from.addEventListener('submit', (event) => {
   if (!emailInput.value.trim()) {
     emailError.style.display = 'block'
     emailError.innerText = 'Поле не заполнено'
-    isValid = false
   } else if (!emailRegex.test(emailInput.value.trim())) {
     emailError.style.display = 'block'
     emailError.innerText = 'Email введен не корректно'
-    isValid = false
   } else {
     emailError.style.display = 'none'
   }
 
+  console.log(iti.getNumber())
+
   if (isValid) {
-    console.log(true)
+    console.log(phoneInput)
   } else {
-    console.log(false)
   }
 })
